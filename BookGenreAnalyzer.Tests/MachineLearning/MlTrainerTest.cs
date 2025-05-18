0f832e6cb3259bc991f3acf314b8be5e36002a47
kamil <kamil.kurzaj04@gmail.com>
2025-05-18 10:44:08 +0200

    Created MlTrainerTest.cs

--- BookGenreAnalyzer.Tests/MachineLearning/MlTrainerTest.cs
+++ BookGenreAnalyzer.Tests/MachineLearning/MlTrainerTest.cs
@@ -1,29 +1,45 @@
-﻿using BookGenreAnalyzer.MachineLearning;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using BookGenreAnalyzer.MachineLearning;
 using Microsoft.ML;
 using Xunit;
 
-namespace BookGenreAnalyzer.Tests.MachineLearning;
-
-public class MlTrainerTest
+namespace BookGenreAnalyzer.Tests.MachineLearning
 {
-   
-    private MlTrainer _mlTrainer;
-    private MLContext _mlContext;
-    private MlDataLoader _mlDataLoader;
-    public MlTrainerTest()
+    public class MlTrainerTest : IDisposable
     {
-        _mlContext = new MLContext(seed: 0);
-        _mlDataLoader = new MlDataLoader();
+        private readonly MlTrainer _mlTrainer;
+        private readonly MLContext _mlContext;
+        private readonly MlDataLoader _mlDataLoader;
+        private string path = @"C:\Users\kamil\RiderProjects\BookGenreAnalyzer\BookGenreAnalyzer\wwwroot\ZIPFiles\generated_stories_english.zip";
+
+        public MlTrainerTest()
+        {
+            _mlContext = new MLContext(seed: 0);
+            _mlDataLoader = new MlDataLoader();
+            _mlTrainer = new MlTrainer(_mlContext, _mlDataLoader);
+            
+        }
+
+        [Fact]
+        public void Should_Create_Valid_Zip_File()
+        {
+            // Act
+            _mlTrainer.TrainAndSaveModel();
+
+            // Assert
+            Assert.True(File.Exists(path), "ZIP file was not created.");
+            Assert.True(new FileInfo(path).Length > 1 * 1024 * 1024, "ZIP file is smaller than 1MB.");
+
+            using var archive = ZipFile.OpenRead(path);
+            Assert.True(archive.Entries.Count > 0, "ZIP archive is empty.");
+        }
         
-        _mlTrainer = new MlTrainer(_mlContext,_mlDataLoader);
-
-
-    }
-
-
-    [Fact]
-    public void BuildAndTrainModel_ShouldReturnNotNullEstimator()
-    {
-        Assert.NotNull(_mlTrainer);
+        public void Dispose()
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+        }
     }
 }
